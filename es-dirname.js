1412734308210f1037c50c9b242c77d19507ef28
vdegenne <vdegenne@gmail.com>
2019-04-17 18:31:23 +0200

    fix the split symbol

--- es-dirname.js
+++ es-dirname.js
@@ -5,7 +5,7 @@ module.exports = () => {
   try {
     ShadowsAlwaysDieTwice
   } catch (e) {
-    const initiator = e.stack.split('at').slice(2, 3)[0]
+    const initiator = e.stack.split('\n').slice(2, 3)[0]
     let path = /(?<path>[^\(]+):[0-9]+:[0-9]+/.exec(initiator).groups.path
     if (path.indexOf('file') >= 0) {
       path = new URL(path).pathname
